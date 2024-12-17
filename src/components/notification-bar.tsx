8d7054aba53720825b2f4af3bc290814f3a10a27
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-17 16:26:41 -0300

    Adds notification bar component

---
+++ src/components/notification-bar.tsx
@@ -0,0 +1,9 @@
+export default function NotificationBar() {
+  return (
+    <div className="bg-primary p-2 text-white text-center cursor-pointer dark:bg-gray-800">
+      <h1 className="font-extralight">
+        Envíos gratis por compras sobre $ 500.000 👏
+      </h1>
+    </div>
+  );
+}
