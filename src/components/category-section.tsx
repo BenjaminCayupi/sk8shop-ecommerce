32d5b769daecc8cd9c84394c6833c9892ffa07e6
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-17 19:38:14 -0300

    Adds category section

---
+++ src/components/category-section.tsx
@@ -0,0 +1,15 @@
+import CategoryCard from "./category-card";
+import SectionTitle from "./section-title";
+
+export default function CategorySection() {
+  return (
+    <section className="mb-8">
+      <SectionTitle title="Categorías" />
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+        <CategoryCard title="skate" imageUrl="" />
+        <CategoryCard title="ropa" imageUrl="" />
+        <CategoryCard title="zapatillas" imageUrl="" />
+      </div>
+    </section>
+  );
+}
