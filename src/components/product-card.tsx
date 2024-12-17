bb4a84608f02bfa5258ebf58e8ca60278e002b29
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-17 16:26:15 -0300

    Adds product card component

---
+++ src/components/product-card.tsx
@@ -0,0 +1,35 @@
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+
+interface Props {
+  title: string;
+  price: number;
+  imageUrl: string;
+  badge?: string;
+}
+
+export default function ProductCard({ title, price, imageUrl, badge }: Props) {
+  return (
+    <Card>
+      <AspectRatio ratio={16 / 9}>
+        <Image
+          src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+          alt="Photo by Drew Beamer"
+          fill
+          className="h-full w-full rounded-md object-cover"
+        />
+        {badge && (
+          <Badge className="absolute top-2 right-2 bg-primary text-primary-foreground dark:bg-black dark:text-white capitalize">
+            {badge}
+          </Badge>
+        )}
+      </AspectRatio>
+      <CardContent className="p-2 text-center">
+        <p className="font-semibold tracking-tight capitalize">{title}</p>
+        <p>$ {price}</p>
+      </CardContent>
+    </Card>
+  );
+}
