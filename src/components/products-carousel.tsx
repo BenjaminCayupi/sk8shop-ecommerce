44b8ca58ede2a63933a94425d9d9967a27575ef9
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-18 14:18:51 -0300

    Update responsive

--- src/components/products-carousel.tsx
+++ src/components/products-carousel.tsx
@@ -27,7 +27,7 @@ export default function ProductsCarousel({ title, products }: Props) {
           {products.map((_, index) => (
             <CarouselItem
               key={index}
-              className="pl-1 md:basis-1/2 lg:basis-1/4"
+              className="pl-1 md:basis-1/2 lg:basis-1/3 xl:basis-1/4"
             >
               <div className="p-1">
                 <ProductCard
@@ -41,11 +41,11 @@ export default function ProductsCarousel({ title, products }: Props) {
           ))}
         </CarouselContent>
 
-        <div className="hidden sm:block">
+        <div className="hidden lg:block">
           <CarouselPrevious />
           <CarouselNext />
         </div>
-        <div className="sm:hidden">
+        <div className="lg:hidden">
           <div className="absolute top-1/2 left-2 flex items-center justify-center">
             <CarouselPrevious className="relative left-0 translate-x-0 hover:translate-x-0 hover:bg-gray-200 dark:hover:bg-gray-700" />
           </div>
