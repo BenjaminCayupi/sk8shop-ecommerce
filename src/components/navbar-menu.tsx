5537d24dfbd967c1b83580db0640d2bc17c1c842
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-19 12:05:29 -0300

    Refactor navbar menu to simplify structure and update link for 'Getting started'

--- src/components/navbar-menu.tsx
+++ src/components/navbar-menu.tsx
@@ -80,54 +80,11 @@ export function NavbarMenu() {
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-2">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <Link
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components
-                    </p>
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                <ul className="mt-2 space-y-2">
-                  <SubListItem href="/docs/overview">Overview</SubListItem>
-                  <SubListItem href="/docs/quickstart">Quickstart</SubListItem>
-                  <SubListItem href="/docs/changelog">Changelog</SubListItem>
-                </ul>
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                <ul className="mt-2 space-y-2">
-                  <SubListItem href="/docs/installation/cli">CLI</SubListItem>
-                  <SubListItem href="/docs/installation/manual">
-                    Manual
-                  </SubListItem>
-                </ul>
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                <ul className="mt-2 space-y-2">
-                  <SubListItem href="/docs/primitives/typography/headings">
-                    Headings
-                  </SubListItem>
-                  <SubListItem href="/docs/primitives/typography/paragraph">
-                    Paragraph
-                  </SubListItem>
-                  <SubListItem href="/docs/primitives/typography/blockquote">
-                    Blockquote
-                  </SubListItem>
-                </ul>
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Inicio
+            </NavigationMenuLink>
+          </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuTrigger>Components</NavigationMenuTrigger>
