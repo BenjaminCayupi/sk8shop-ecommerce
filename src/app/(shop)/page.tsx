cbccc9f52b1f026d2c236eeb1adf21f61183ac70
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-19 21:35:48 -0300

    Remove unused Footer import from Shop page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,4 @@
 import BannerSection from "@/components/sections/banner-section";
-import Footer from "@/components/footer";
 import HeroSection from "@/components/sections/hero-section";
 import ProductsCarousel from "@/components/products-carousel";
 import CategorySection from "@/components/sections/category-section";
