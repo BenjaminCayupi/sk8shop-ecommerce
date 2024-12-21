42581e90a75c8d6d420c4e49d330aa043eb0781f
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-21 02:05:42 -0300

    Add LoginPage and AuthCard components for user authentication

---
+++ src/app/(shop)/login/page.tsx
@@ -0,0 +1,9 @@
+import { AuthCard } from "@/components/auth-card";
+
+export default function LoginPage() {
+  return (
+    <div className="container flex justify-center mt-[50px]">
+      <AuthCard />
+    </div>
+  );
+}
---
+++ src/components/auth-card.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Icons } from "./icons";
+
+export function AuthCard() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className="animate-fade">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Autenticación</CardTitle>
+          <CardDescription>Inicia sesión o crea una cuenta</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Tabs defaultValue="login" className="w-full">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="login">Iniciar Sesión</TabsTrigger>
+              <TabsTrigger value="register">Registrarse</TabsTrigger>
+            </TabsList>
+            <TabsContent value="login">
+              <form onSubmit={onSubmit}>
+                <div className="grid gap-2">
+                  <div className="grid gap-1">
+                    <Label className="sr-only" htmlFor="email">
+                      Email
+                    </Label>
+                    <Input
+                      id="email"
+                      placeholder="nombre@ejemplo.com"
+                      type="email"
+                      autoCapitalize="none"
+                      autoComplete="email"
+                      autoCorrect="off"
+                      disabled={isLoading}
+                    />
+                  </div>
+                  <div className="grid gap-1">
+                    <Label className="sr-only" htmlFor="password">
+                      Contraseña
+                    </Label>
+                    <Input
+                      id="password"
+                      placeholder="Contraseña"
+                      type="password"
+                      autoCapitalize="none"
+                      autoComplete="current-password"
+                      autoCorrect="off"
+                      disabled={isLoading}
+                    />
+                  </div>
+                  <Button disabled={isLoading}>
+                    {isLoading && (
+                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Iniciar Sesión
+                  </Button>
+                </div>
+              </form>
+            </TabsContent>
+            <TabsContent value="register">
+              <form onSubmit={onSubmit}>
+                <div className="grid gap-2">
+                  <div className="grid gap-1">
+                    <Label className="sr-only" htmlFor="name">
+                      Nombre
+                    </Label>
+                    <Input
+                      id="name"
+                      placeholder="Nombre"
+                      type="text"
+                      autoCapitalize="none"
+                      autoComplete="name"
+                      autoCorrect="off"
+                      disabled={isLoading}
+                    />
+                  </div>
+                  <div className="grid gap-1">
+                    <Label className="sr-only" htmlFor="email">
+                      Email
+                    </Label>
+                    <Input
+                      id="email"
+                      placeholder="nombre@ejemplo.com"
+                      type="email"
+                      autoCapitalize="none"
+                      autoComplete="email"
+                      autoCorrect="off"
+                      disabled={isLoading}
+                    />
+                  </div>
+                  <div className="grid gap-1">
+                    <Label className="sr-only" htmlFor="password">
+                      Contraseña
+                    </Label>
+                    <Input
+                      id="password"
+                      placeholder="Contraseña"
+                      type="password"
+                      autoCapitalize="none"
+                      autoComplete="new-password"
+                      autoCorrect="off"
+                      disabled={isLoading}
+                    />
+                  </div>
+                  <div className="grid gap-1">
+                    <Label className="sr-only" htmlFor="confirm-password">
+                      Confirmar Contraseña
+                    </Label>
+                    <Input
+                      id="confirm-password"
+                      placeholder="Confirmar Contraseña"
+                      type="password"
+                      autoCapitalize="none"
+                      autoComplete="new-password"
+                      autoCorrect="off"
+                      disabled={isLoading}
+                    />
+                  </div>
+                  <Button disabled={isLoading}>
+                    {isLoading && (
+                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Crear cuenta
+                  </Button>
+                </div>
+              </form>
+            </TabsContent>
+          </Tabs>
+        </CardContent>
+        <CardFooter>
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            Al hacer click, estas aceptando nuestros{" "}
+            <a
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Términos y Condiciones
+            </a>{" "}
+            y{" "}
+            <a
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Política de privacidad
+            </a>
+            .
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
