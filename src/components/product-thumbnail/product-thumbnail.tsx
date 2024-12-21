8ddef3602da5f02f999819f7737eeb7ec9533853
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-21 16:37:05 -0300

    Add ProductThumbnail component with carousel functionality and styles

---
+++ src/components/product-thumbnail/product-thumbnail.tsx
@@ -0,0 +1,82 @@
+"use client";
+import React, { useState, useEffect, useCallback } from "react";
+import { EmblaOptionsType } from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
+import { Thumb } from "./thumb-buttons";
+import "./styles.css";
+import { AspectRatio } from "../ui/aspect-ratio";
+import Image from "next/image";
+type PropType = {
+  slides: number[];
+  options?: EmblaOptionsType;
+};
+
+const ProductThumbnail: React.FC<PropType> = (props) => {
+  const { slides, options } = props;
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options);
+  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
+    containScroll: "keepSnaps",
+    dragFree: true,
+  });
+
+  const onThumbClick = useCallback(
+    (index: number) => {
+      if (!emblaMainApi || !emblaThumbsApi) return;
+      emblaMainApi.scrollTo(index);
+    },
+    [emblaMainApi, emblaThumbsApi]
+  );
+
+  const onSelect = useCallback(() => {
+    if (!emblaMainApi || !emblaThumbsApi) return;
+    setSelectedIndex(emblaMainApi.selectedScrollSnap());
+    emblaThumbsApi.scrollTo(emblaMainApi.selectedScrollSnap());
+  }, [emblaMainApi, emblaThumbsApi, setSelectedIndex]);
+
+  useEffect(() => {
+    if (!emblaMainApi) return;
+    onSelect();
+
+    emblaMainApi.on("select", onSelect).on("reInit", onSelect);
+  }, [emblaMainApi, onSelect]);
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaMainRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <AspectRatio ratio={4 / 3} className="w-full ">
+                <Image
+                  src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+                  alt="Photo by Drew Beamer"
+                  fill
+                  className="object-cover rounded-md"
+                  sizes="100%"
+                />
+              </AspectRatio>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla-thumbs">
+        <div className="embla-thumbs__viewport" ref={emblaThumbsRef}>
+          <div className="embla-thumbs__container">
+            {slides.map((index) => (
+              <Thumb
+                key={index}
+                onClick={() => onThumbClick(index)}
+                selected={index === selectedIndex}
+                index={index}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductThumbnail;
---
+++ src/components/product-thumbnail/thumb-buttons.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { AspectRatio } from "../ui/aspect-ratio";
+
+type PropType = {
+  selected: boolean;
+  index: number;
+  onClick: () => void;
+};
+
+export const Thumb: React.FC<PropType> = (props) => {
+  const { selected, onClick } = props;
+
+  return (
+    <div
+      className={"embla-thumbs__slide".concat(
+        selected ? " embla-thumbs__slide--selected" : ""
+      )}
+    >
+      <button
+        onClick={onClick}
+        type="button"
+        className="embla-thumbs__slide__number"
+      >
+        <AspectRatio ratio={4 / 3} className="w-full ">
+          <Image
+            src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+            alt="Photo by Drew Beamer"
+            fill
+            className="object-cover rounded-md"
+            sizes="100%"
+          />
+        </AspectRatio>
+      </button>
+    </div>
+  );
+};
