772ce31a629511141387bb48581677697684e297
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-23 17:38:46 -0300

    Enhance Admin layout with sidebar and breadcrumb navigation; update AdminPage title

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
-export default function AdminPage() {
+export default function Page() {
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="container ">
+      <h1 className="text-3xl font-bold mb-8">Bienvenido, BENJAMIN</h1>
     </div>
   );
 }
