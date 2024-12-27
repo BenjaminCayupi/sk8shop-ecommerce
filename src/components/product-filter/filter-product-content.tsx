0bf064836a4a1941519d3984fd9c2203beb25d9c
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-27 12:46:52 -0300

    Enhance product filter UI by adding checkboxes for availability and size options

--- src/components/product-filter/filter-product-content.tsx
+++ src/components/product-filter/filter-product-content.tsx
@@ -14,8 +14,16 @@ export default function FilterProductContent() {
         <AccordionTrigger className="hover:no-underline">
           Disponibilidad
         </AccordionTrigger>
-        <AccordionContent className="pb-4">
-          <p>asd</p>
+        <AccordionContent className="pb-4 space-y-2">
+          <div className="flex items-center space-x-2">
+            <Checkbox id="creature" />
+            <Label
+              htmlFor="creature"
+              className="text-sm font-normal capitalize"
+            >
+              Creature (37)
+            </Label>
+          </div>
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="marca" className="border-b">
@@ -37,38 +45,18 @@ export default function FilterProductContent() {
 
       <AccordionItem value="medida" className="border-b">
         <AccordionTrigger className="hover:no-underline">
-          Medida
-        </AccordionTrigger>
-        <AccordionContent className="pb-4">
-          <p>asd</p>
-        </AccordionContent>
-      </AccordionItem>
-
-      <AccordionItem value="concavo" className="border-b">
-        <AccordionTrigger className="hover:no-underline">
-          Concavo
+          Talla / Medida
         </AccordionTrigger>
         <AccordionContent className="pb-4 space-y-2">
           <div className="flex items-center space-x-2">
-            <Checkbox id="high" />
-            <Label htmlFor="high" className="text-sm font-normal capitalize">
-              High (2)
+            <Checkbox id="creature" />
+            <Label
+              htmlFor="creature"
+              className="text-sm font-normal capitalize"
+            >
+              Creature (37)
             </Label>
           </div>
-          <div className="flex items-center space-x-2">
-            <Checkbox id="med" />
-            <Label htmlFor="med" className="text-sm font-normal capitalize">
-              Med (32)
-            </Label>
-          </div>
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="tecnologia" className="border-b">
-        <AccordionTrigger className="hover:no-underline">
-          Tecnologia
-        </AccordionTrigger>
-        <AccordionContent className="pb-4">
-          <p>asd</p>
         </AccordionContent>
       </AccordionItem>
     </Accordion>
