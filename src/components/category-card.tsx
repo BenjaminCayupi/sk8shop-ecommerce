02f1abb74e15196e8004f8c9bdfa90c75ac54055
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-27 12:46:45 -0300

    Wrap CategoryCard component in a Link for navigation to the products page

--- src/components/category-card.tsx
+++ src/components/category-card.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 interface CategoryCardProps {
   title: string;
@@ -12,26 +13,28 @@ export default function CategoryCard({ title, imageUrl }: CategoryCardProps) {
     ? imageUrl
     : "/imgs/placeholder-image.jpg";
   return (
-    <Card className="w-full overflow-hidden cursor-pointer hover:opacity-90 transition-all">
-      <div className="relative">
-        <AspectRatio ratio={3 / 3} className="hover:scale-125">
-          <Image
-            src={url}
-            alt={title}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="object-cover"
-          />
-        </AspectRatio>
-        <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
-        <div className="absolute bottom-0 left-0 right-0 backdrop-blur-md bg-black/30">
-          <CardContent className="p-7 text-center">
-            <CardTitle className="text-lg mb-1 text-white dark:text-gray-100 truncate uppercase">
-              {title}
-            </CardTitle>
-          </CardContent>
+    <Link href="/products/skate">
+      <Card className="w-full overflow-hidden cursor-pointer hover:opacity-90 transition-all">
+        <div className="relative">
+          <AspectRatio ratio={3 / 3} className="hover:scale-125">
+            <Image
+              src={url}
+              alt={title}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="object-cover"
+            />
+          </AspectRatio>
+          <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
+          <div className="absolute bottom-0 left-0 right-0 backdrop-blur-md bg-black/30">
+            <CardContent className="p-7 text-center">
+              <CardTitle className="text-lg mb-1 text-white dark:text-gray-100 truncate uppercase">
+                {title}
+              </CardTitle>
+            </CardContent>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 }
