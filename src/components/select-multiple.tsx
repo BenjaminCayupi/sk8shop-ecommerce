41c575b344394b119b7237edc243ef30c6b9f397
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-27 12:33:57 -0300

    Add SizeSelect component

---
+++ src/components/select-multiple.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useState } from "react";
+import MultipleSelector, { Option } from "./ui/multiple-selector";
+import { Input } from "./ui/input";
+import { Badge } from "./ui/badge";
+
+const OPTIONS: Option[] = [
+  { label: "S", value: "nextjs" },
+  { label: "M", value: "react" },
+  { label: "L", value: "remix" },
+  { label: "XL", value: "vite" },
+];
+
+const SizeSelect = ({}) => {
+  const [value, setValue] = useState<Option[]>([]);
+  console.log("value :", value);
+  return (
+    <div className="w-full">
+      <MultipleSelector
+        value={value}
+        onChange={setValue}
+        defaultOptions={OPTIONS}
+        hidePlaceholderWhenSelected
+        placeholder="Seleccionar tallas"
+        creatable
+        emptyIndicator={
+          <p className="text-center text-sm  text-gray-600 dark:text-gray-400">
+            No quedan tallas
+          </p>
+        }
+      />
+      <p className="text-xs ml-2 mt-1 text-slate-500">
+        Puede seleccionar múltiples tallas o comenzar a escribir para crear una
+        nueva
+      </p>
+      {value.length > 0 &&
+        value.map((item) => (
+          <div
+            key={item.label}
+            className="flex flex-row mt-4 align-middle justify-between"
+          >
+            <div className="w-1/6 flex">
+              <Badge variant="outline" className="w-4/6 flex justify-center">
+                {item.label}
+              </Badge>
+            </div>
+            <Input
+              id="name"
+              value=""
+              readOnly
+              className="w-5/6"
+              placeholder="Cantidad"
+            />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default SizeSelect;
