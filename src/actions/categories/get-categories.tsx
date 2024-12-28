248f2516e61a2b04c30ce39524a88440961e8586
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-12-28 15:22:27 -0300

    Add getCategories server action

---
+++ src/actions/categories/get-categories.tsx
@@ -0,0 +1,46 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+interface PaginationOptions {
+  page?: number;
+  rowsPerPage?: number;
+  enabled?: boolean;
+}
+
+export async function getCategories({
+  page = 1,
+  rowsPerPage = 2,
+  enabled,
+}: PaginationOptions) {
+  try {
+    const [categories, count] = await Promise.all([
+      prisma.category.findMany({
+        orderBy: { title: "asc" },
+        where: { enabled },
+        skip: (page - 1) * rowsPerPage,
+        take: rowsPerPage,
+      }),
+      prisma.category.count(),
+    ]);
+
+    return {
+      categories,
+      paginationOptions: {
+        currentPage: page,
+        totalCount: count,
+        totalPages: Math.ceil(count / rowsPerPage),
+        rowsPerPage,
+      },
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        error: true,
+        message: error.message,
+      };
+    }
+
+    return { error: true, message: "Ha ocurrido un error" };
+  }
+}
