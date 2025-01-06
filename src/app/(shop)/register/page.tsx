96627ee0b009a9a7a550559785688e75198c14e7
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-06 15:42:33 -0300

    Redirect authenticated users from the registration page

--- src/app/(shop)/register/page.tsx
+++ src/app/(shop)/register/page.tsx
@@ -1,6 +1,14 @@
+import { auth } from "@/auth/auth";
 import RegisterForm from "@/components/forms/register-form";
+import { redirect } from "next/navigation";
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/");
+  }
+
   return (
     <div className="container mt-10 flex justify-center">
       <RegisterForm />
