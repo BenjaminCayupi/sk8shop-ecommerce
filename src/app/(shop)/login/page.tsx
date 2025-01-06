ecafc893ff8702a2303b1be620ef59ebf84e1009
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-06 17:10:00 -0300

    Refactor login page to use LoginForm component and update redirect logic

--- src/app/(shop)/login/page.tsx
+++ src/app/(shop)/login/page.tsx
@@ -1,84 +1,16 @@
 import { auth } from "@/auth/auth";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@radix-ui/react-label";
-import Link from "next/link";
+import LoginForm from "@/components/forms/login-form";
 import { redirect } from "next/navigation";
 
 export default async function LoginPage() {
   const session = await auth();
 
   if (session?.user) {
-    redirect("/profile");
+    redirect("/");
   }
   return (
     <div className="container mt-10 flex justify-center">
-      <div className="flex flex-col gap-6">
-        <Card className="motion-preset-slide-up">
-          <CardHeader>
-            <CardTitle className="text-2xl">Bienvenido</CardTitle>
-            <CardDescription>
-              Haz login con tus credenciales o crea una nueva cuenta
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="grid gap-6">
-                <div className="grid gap-4">
-                  <div className="grid gap-2">
-                    <Label htmlFor="email" className="font-medium">
-                      Email
-                    </Label>
-                    <Input
-                      id="email"
-                      type="email"
-                      placeholder="m@ejemplo.com"
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-2">
-                    <div className="flex items-center">
-                      <Label htmlFor="password" className="font-medium">
-                        Contraseña
-                      </Label>
-                      <a
-                        href="#"
-                        className="ml-auto text-sm underline-offset-4 hover:underline"
-                      >
-                        Olvidaste tu contraseña?
-                      </a>
-                    </div>
-                    <Input id="password" type="password" required />
-                  </div>
-                  <Button type="submit" className="w-full">
-                    Login
-                  </Button>
-                </div>
-                <div className="text-center text-sm">
-                  No tienes una cuenta?{" "}
-                  <Link
-                    href="/register"
-                    className="underline underline-offset-4"
-                  >
-                    Regístrate
-                  </Link>
-                </div>
-              </div>
-            </form>
-          </CardContent>
-        </Card>
-        <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
-          By clicking continue, you agree to our{" "}
-          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
-        </div>
-      </div>
+      <LoginForm />
     </div>
   );
 }
