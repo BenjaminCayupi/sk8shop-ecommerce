5f9db162129f73c0e5c65618c1cd68937664294c
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-07 11:34:52 -0300

    Add validation schemas for login and registration forms

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -6,6 +6,15 @@ export const loginSchema = object({
     .email("Invalid email"),
   password: string({ required_error: "Password is required" })
     .min(1, "Password is required")
+    .min(8, "Password must be more than 8 characters"),
+});
+
+export const registerSchema = object({
+  email: string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Invalid email"),
+  password: string({ required_error: "Password is required" })
+    .min(1, "Password is required")
     .min(8, "Password must be more than 8 characters")
     .refine(
       (value) =>
