c4ff62e1bc5c4491bda1f873275f1c678c75eedf
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-09 15:02:51 -0300

    Add utility functions for parameter validation and default values

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,3 +1,31 @@
 export const imageSrc = (url: string): string => {
   return url.startsWith("https") ? url : "/imgs/placeholder-image.jpg";
 };
+
+export const paramNumber = (
+  param: string | undefined,
+  defaultValue?: number
+) => {
+  if (defaultValue) {
+    if (!param) return defaultValue;
+
+    return isNaN(+param) ? defaultValue : +param;
+  }
+  if (!param) return 1;
+
+  return isNaN(+param) ? 1 : +param;
+};
+
+type rowsPerPage = 5 | 10 | 15;
+
+export const paramTake = (param: string | undefined): rowsPerPage => {
+  if (!param) return 5;
+
+  const valid = +param;
+
+  if (isNaN(valid)) return 5;
+
+  if (valid !== 5 && valid !== 10 && valid !== 15) return 5;
+
+  return valid as rowsPerPage;
+};
