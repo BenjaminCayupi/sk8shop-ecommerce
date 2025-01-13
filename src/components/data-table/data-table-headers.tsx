7026251246d50f079f6e7c2fdc577c8cd414e347
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-13 10:34:52 -0300

    Implement sorting functionality in DataTableHeaders component

--- src/components/data-table/data-table-headers.tsx
+++ src/components/data-table/data-table-headers.tsx
@@ -2,6 +2,8 @@
 import { ArrowUpDown } from "lucide-react";
 import { Button } from "../ui/button";
 import { TableHead, TableHeader, TableRow } from "../ui/table";
+import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 interface Props {
   headers: {
@@ -11,13 +13,30 @@ interface Props {
 }
 
 export default function DataTableHeaders({ headers }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const [sortDirection, setSortDirection] = useState(true);
+
+  const onSort = (key: string) => {
+    setSortDirection(!sortDirection);
+    params.set("page", "1");
+    params.set("sortBy", key);
+    params.set("sortDirection", sortDirection ? "asc" : "desc");
+    router.push(`${pathname}?${params.toString()}`);
+  };
   return (
     <TableHeader>
       <TableRow>
         {headers.map((header) =>
           header.key ? (
             <TableHead key={header.title}>
-              <Button variant="ghost" className="text-left capitalize px-0">
+              <Button
+                variant="ghost"
+                className="text-left capitalize"
+                onClick={() => onSort(header.key)}
+              >
                 {header.title}
                 <ArrowUpDown />
               </Button>
