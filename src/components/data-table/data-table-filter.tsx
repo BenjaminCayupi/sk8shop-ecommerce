07f97ebcbb4817cd681daffbecdbfab0e45b59e1
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-13 09:45:06 -0300

    Implement search functionality in DataTableFilter component

--- src/components/data-table/data-table-filter.tsx
+++ src/components/data-table/data-table-filter.tsx
@@ -1,20 +1,30 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Search } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function DataTableFilter() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [value, setValue] = useState("");
+
+  const search = () => {
+    router.push(`${pathname}/?query=${value}`);
+  };
+
   return (
     <div className="flex flex-row">
       <Input
         type="text"
         placeholder="Filtrar"
         className="mr-2"
-        /* value={value}
-        onChange={onChangeFunc} */
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
-      <Button type="submit">
+      <Button onClick={search}>
         <Search />
       </Button>
     </div>
