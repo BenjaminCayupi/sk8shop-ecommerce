9ae68c68c094dbf24f27465ab67e268155f42232
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-15 14:30:30 -0300

    Adds get form data server action for products

---
+++ src/actions/products/get-form-data.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+export async function getFormData() {
+  try {
+    const brands = await prisma.brand.findMany({
+      where: { enabled: true },
+      orderBy: { title: "asc" },
+    });
+
+    const subCategories = await prisma.subCategory.findMany({
+      where: { enabled: true },
+      orderBy: { title: "asc" },
+    });
+
+    const sizes = await prisma.size.findMany({
+      orderBy: { title: "asc" },
+    });
+
+    return {
+      data: { brands, subCategories, sizes },
+    };
+  } catch (error) {
+    console.log("error :", error);
+    return {
+      ok: false,
+    };
+  }
+}
