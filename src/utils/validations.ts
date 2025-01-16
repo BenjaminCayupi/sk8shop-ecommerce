ecbe17917fc97cce2d9487a545e6d25b239274c0
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-16 13:14:02 -0300

    Add createSlug utility function for title formatting

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -29,3 +29,14 @@ export const paramTake = (param: string | undefined): rowsPerPage => {
 
   return valid as rowsPerPage;
 };
+
+export const createSlug = (title: string) => {
+  return title
+    ? title
+        .toLowerCase() // Convert to lowercase
+        .trim() // Remove whitespace from both ends
+        .replace(/[^a-z0-9\s-]/g, "") // Remove special characters
+        .replace(/\s+/g, "-") // Replace spaces with hyphens
+        .replace(/-+/g, "-")
+    : ""; // Replace multiple hyphens with a single one
+};
