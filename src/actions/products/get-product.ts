8c801261aca386483c4f9b13fe4b01753f282810
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-17 23:58:12 -0300

    Add getProduct server action

---
+++ src/actions/products/get-product.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+export async function getProduct(id: number) {
+  console.log("id :", id);
+  try {
+    const product = await prisma.product.findUnique({
+      where: { id: +id },
+      include: {
+        Inventory: {
+          include: {
+            size: true,
+          },
+        },
+      },
+    });
+    return {
+      ok: true,
+      data: product,
+      message: "",
+    };
+  } catch (error) {
+    console.log("error :", error);
+    return {
+      ok: false,
+      message: "Ha ocurrido un error",
+    };
+  }
+}
