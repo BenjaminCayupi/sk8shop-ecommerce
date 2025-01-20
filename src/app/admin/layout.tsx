d7b76b24ed78020a5d0c39f423aafd9b8144db9e
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-20 12:21:45 -0300

    Implement sidebar toggle functionality with localStorage persistence

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { AppSidebar } from "@/components/app-sidebar";
 import {
   Breadcrumb,
@@ -13,19 +14,33 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { Separator } from "@radix-ui/react-separator";
+import { useState } from "react";
 
 export default function ShopLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [sidebarOpen, setSidebarOpen] = useState(() => {
+    const storedValue = localStorage.getItem("sidebarOpen");
+    return storedValue ? JSON.parse(storedValue) : true;
+  });
+
+  const toggleSidebar = () => {
+    setSidebarOpen((prev: boolean) => {
+      const newValue = !prev;
+      localStorage.setItem("sidebarOpen", JSON.stringify(newValue)); // Update localStorage
+      return newValue;
+    });
+  };
+
   return (
-    <SidebarProvider>
+    <SidebarProvider open={sidebarOpen}>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+            <SidebarTrigger className="-ml-1" onClick={toggleSidebar} />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
               <BreadcrumbList>
