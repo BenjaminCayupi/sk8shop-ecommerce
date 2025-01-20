4b56a2b30bcd984ea97ae9ac0344930cbcf76d45
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-01-20 12:24:29 -0300

    Enhance product management: add inventory handling, error messages, and pagination to products page

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,4 +1,5 @@
 import { getFormData } from "@/actions/products/get-form-data";
+import { getProducts } from "@/actions/products/get-products";
 import DataTableFilter from "@/components/data-table/data-table-filter";
 import DataTableHeaders from "@/components/data-table/data-table-headers";
 import DataTablePagination from "@/components/data-table/data-table-pagination";
@@ -6,31 +7,52 @@ import { ProductForm } from "@/components/forms/product-form";
 import PageTitle from "@/components/page-title";
 import { Card } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { paramNumber, paramTake } from "@/utils";
+import { Check, X } from "lucide-react";
 
-const testData = [
-  { id: 1, name: "title" },
-  { id: 1, name: "title" },
-  { id: 1, name: "title" },
-  { id: 1, name: "title" },
-  { id: 1, name: "title" },
+const productsHeaders = [
+  { title: "id", key: "id" },
+  { title: "nombre", key: "title" },
+  { title: "marca", key: "brand" },
+  { title: "SubCategoría", key: "subCategory" },
+  { title: "habilitado", key: "enabled" },
 ];
 
-export default async function ProductsPage() {
-  const productsHeaders = [
-    { title: "id", key: "id" },
-    { title: "nombre", key: "name" },
-    { title: "habilitado", key: "enabled" },
-  ];
+interface Props {
+  searchParams: {
+    page?: string;
+    take?: string;
+    query?: string;
+    sortBy?: string;
+    sortDirection?: string;
+  };
+}
 
-  const { brands, subCategories, sizes } = await Promise.all([
-    getFormData(),
-  ]).then((results) => {
-    return {
-      brands: results[0].data?.brands,
-      subCategories: results[0].data?.subCategories,
-      sizes: results[0].data?.sizes,
-    };
-  });
+export default async function ProductsPage({ searchParams }: Props) {
+  const param = await searchParams;
+
+  const page = paramNumber(param.page);
+  const take = paramTake(param.take);
+
+  const { data, paginationOptions, brands, subCategories, sizes } =
+    await Promise.all([
+      getProducts({
+        page,
+        rowsPerPage: take,
+        query: param?.query,
+        sortBy: param?.sortBy,
+        sortDirection: param?.sortDirection,
+      }),
+      getFormData(),
+    ]).then((results) => {
+      return {
+        data: results[0].data,
+        paginationOptions: results[0].paginationOptions,
+        brands: results[1].data?.brands,
+        subCategories: results[1].data?.subCategories,
+        sizes: results[1].data?.sizes,
+      };
+    });
 
   return (
     <div className="container">
@@ -52,30 +74,45 @@ export default async function ProductsPage() {
         <Table>
           <DataTableHeaders headers={productsHeaders} />
           <TableBody>
-            {testData.map((item, index) => (
-              <TableRow key={index}>
-                <TableCell className="font-medium">{item.id}</TableCell>
-                <TableCell>Test name</TableCell>
-                <TableCell>test</TableCell>
-                <TableCell className="flex flex-row justify-end">
-                  <ProductForm
-                    isEdit={true}
-                    brands={brands}
-                    subCategories={subCategories}
-                    sizes={sizes}
-                  />
-                  {/*  <Button
-                    className="bg-red-500 hover:bg-red-700 ml-2"
-                    size="icon"
-                  >
-                    <Trash />
-                  </Button> */}
+            {data?.length ? (
+              data.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">{item.id}</TableCell>
+                  <TableCell>{item.title}</TableCell>
+                  <TableCell className="capitalize">
+                    {item.brand.title}
+                  </TableCell>
+                  <TableCell className="capitalize">
+                    {item.subCategory.title}
+                  </TableCell>
+                  <TableCell>{item.enabled ? <Check /> : <X />}</TableCell>
+                  <TableCell className="flex flex-row justify-end">
+                    <ProductForm
+                      isEdit={true}
+                      id={item.id}
+                      brands={brands}
+                      subCategories={subCategories}
+                      sizes={sizes}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  className="font-medium text-center text-gray-500"
+                  colSpan={4}
+                >
+                  No hay registros
                 </TableCell>
               </TableRow>
-            ))}
+            )}
           </TableBody>
         </Table>
-        <DataTablePagination />
+        <DataTablePagination
+          currentPage={page}
+          totalPages={paginationOptions?.totalPages}
+        />
       </Card>
     </div>
   );
