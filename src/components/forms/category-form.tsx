899f81b415984fbe737edeecec1f208b60b8321c
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-02-04 21:27:27 -0300

    Refactor CategoryForm

--- src/components/forms/category-form.tsx
+++ src/components/forms/category-form.tsx
@@ -21,6 +21,8 @@ import { Textarea } from "../ui/textarea";
 import { createUpdateCategory } from "@/actions/categories/create-update-category";
 import { getCategory } from "@/actions/categories/get-category";
 import toast from "react-hot-toast";
+import FormDialog from "./form-dialog";
+import FieldContainer from "./field-container";
 
 interface Props {
   isEdit: boolean;
@@ -34,178 +36,139 @@ type Inputs = {
 };
 
 export function CategoryForm({ isEdit, id }: Props) {
-  const [loading, setLoading] = useState(false);
-  const [formLoading, setFormLoading] = useState(false);
-  const [open, setOpen] = useState(false);
+  const [{ loading, formLoading, open }, setStates] = useState({
+    loading: false,
+    formLoading: false,
+    open: false,
+  });
 
   const {
     register,
     handleSubmit,
     formState: { errors },
     control,
-    reset,
+    reset: resetForm,
     setValue,
   } = useForm<Inputs>();
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    setLoading(true);
-    let response;
+    setStates((prev) => ({ ...prev, loading: true }));
 
-    if (isEdit) {
-      response = await createUpdateCategory({ ...data, id });
-    } else {
-      response = await createUpdateCategory(data);
-    }
+    const response = await createUpdateCategory(
+      isEdit ? { ...data, id } : data
+    );
 
     if (!response.ok) {
       toast.error(response.message);
-      setLoading(false);
-      reset();
+      setStates((prev) => ({ ...prev, loading: false }));
       return;
     }
 
-    setLoading(false);
-    setOpen(false);
-    reset();
+    setStates((prev) => ({ ...prev, loading: false, open: false }));
+    resetForm();
     toast.success(response.message);
   };
 
   const editModel = async (id: number) => {
-    setOpen(true);
-    setFormLoading(true);
+    setStates((prev) => ({ ...prev, open: true, formLoading: true }));
 
     const response = await getCategory(id);
 
     if (!response.ok) {
       toast.error(response.message);
+      return;
     }
 
-    setValue("title", response.data!.title, { shouldValidate: true });
-    setValue("description", response.data!.description, {
-      shouldValidate: true,
-    });
-    setValue("enabled", response.data!.enabled, {
-      shouldValidate: true,
+    const { data } = response;
+
+    const valuesToSet = {
+      title: data!.title,
+      description: data!.description,
+      enabled: data!.enabled,
+    };
+
+    Object.entries(valuesToSet).forEach(([key, value]) => {
+      setValue(key as keyof typeof valuesToSet, value, {
+        shouldValidate: true,
+      });
     });
 
-    setFormLoading(false);
+    setStates((prev) => ({ ...prev, formLoading: false }));
   };
 
   return (
-    <Dialog open={open} onOpenChange={(value) => setOpen(value)}>
-      <DialogTrigger asChild>
-        {isEdit ? (
-          <Button size="icon" onClick={() => id !== undefined && editModel(id)}>
-            <Edit />
-          </Button>
-        ) : (
-          <Button onClick={() => setOpen(true)}>
-            <Plus />
-            Agregar
-          </Button>
-        )}
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <DialogHeader>
-            <DialogTitle>{`${
-              isEdit ? "Editar" : "Crear"
-            } Categoría`}</DialogTitle>
-            <DialogDescription>
-              Haz cambios en tus categorías aquí. Oprime guardar cuando estes
-              listo.
-            </DialogDescription>
-          </DialogHeader>
-          {formLoading ? (
-            <div className="w-full h-[250px] content-center justify-items-center">
-              <Loader2 className="motion-preset-spin" size={50} />
-            </div>
-          ) : (
-            <div className="grid gap-4 py-4">
-              <div className="grid items-center gap-3">
-                <Label htmlFor="name" className="text-left">
-                  Nombre
-                </Label>
-                <Input
-                  id="name"
-                  className="col-span-3"
-                  {...register("title", {
-                    required: "El campo es requerido.",
-                    minLength: {
-                      value: 4,
-                      message: "Mínimo 4 caracteres.",
-                    },
-                  })}
-                />
-                <p className="text-sm text-red-400 w-full">
-                  {errors.title?.message}
-                </p>
-              </div>
+    <FormDialog
+      open={open}
+      onOpenChange={(open) => setStates((prev) => ({ ...prev, open }))}
+      isEdit={isEdit}
+      id={id}
+      toggleEdit={editModel}
+      openModal={() => setStates((prev) => ({ ...prev, open: true }))}
+      loading={loading}
+      formLoading={formLoading}
+      closeForm={() => {
+        setStates((prev) => ({ ...prev, open: false }));
+        resetForm();
+      }}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="grid gap-4 py-4">
+        <div className="grid items-center gap-3">
+          <FieldContainer label="Nombre" error={errors.title?.message}>
+            <Input
+              id="name"
+              className="col-span-3"
+              {...register("title", {
+                required: "El campo es requerido.",
+                minLength: {
+                  value: 4,
+                  message: "Mínimo 4 caracteres.",
+                },
+              })}
+            />
+          </FieldContainer>
+        </div>
 
-              <div className="grid items-center gap-3">
-                <Label htmlFor="description" className="text-left">
-                  Descripción
-                </Label>
-                <Textarea
-                  placeholder="Descripción de la categoría"
-                  className="col-span-3"
-                  {...register("description", {
-                    required: "El campo es requerido.",
-                    minLength: {
-                      value: 4,
-                      message: "Mínimo 4 caracteres.",
-                    },
-                    maxLength: {
-                      value: 40,
-                      message: "Máximo 40 caracteres.",
-                    },
-                  })}
-                />
-                <p className="text-sm text-red-400">
-                  {errors.description?.message}
-                </p>
-              </div>
+        <div className="grid items-center gap-3">
+          <FieldContainer
+            label="Descripción"
+            error={errors.description?.message}
+          >
+            <Textarea
+              placeholder="Descripción de la categoría"
+              className="col-span-3"
+              {...register("description", {
+                required: "El campo es requerido.",
+                minLength: {
+                  value: 4,
+                  message: "Mínimo 4 caracteres.",
+                },
+                maxLength: {
+                  value: 40,
+                  message: "Máximo 40 caracteres.",
+                },
+              })}
+            />
+          </FieldContainer>
+        </div>
 
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="airplane-mode">Habilitado</Label>
-                <Controller
-                  control={control}
-                  name="enabled"
-                  defaultValue={false}
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <Switch
-                      onCheckedChange={onChange}
-                      onBlur={onBlur}
-                      checked={value}
-                    />
-                  )}
+        <div className="grid grid-cols-4 items-center gap-4">
+          <FieldContainer label="Habilitado">
+            <Controller
+              control={control}
+              name="enabled"
+              defaultValue={false}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Switch
+                  onCheckedChange={onChange}
+                  onBlur={onBlur}
+                  checked={value}
                 />
-              </div>
-            </div>
-          )}
-
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button
-                type="button"
-                variant="secondary"
-                className="mr-2"
-                disabled={loading}
-                onClick={() => {
-                  reset();
-                  setOpen(false);
-                }}
-              >
-                Cerrar
-              </Button>
-            </DialogClose>
-            <Button type="submit" disabled={loading}>
-              {loading && <Loader2 className="animate-spin" />}
-              Guardar
-            </Button>
-          </DialogFooter>
-        </form>
-      </DialogContent>
-    </Dialog>
+              )}
+            />
+          </FieldContainer>
+        </div>
+      </div>
+    </FormDialog>
   );
 }
