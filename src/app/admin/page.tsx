03254044623adbaeb9793107ee706a1ea4d8a156
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-02-17 12:57:09 -0300

    Implement admin role check and redirect to login for unauthorized access

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,12 @@
-export default function Page() {
+import { auth } from "@/auth/auth";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const session = await auth();
+
+  if (session?.user.role !== "admin") {
+    redirect("/login");
+  }
   return (
     <div className="container ">
       <h1 className="text-3xl font-bold mb-8">Bienvenido, BENJAMIN</h1>
